ffe7205bed3b356506675584c6d7799d4969f833
Baptiste LECHAT <baptiste.lechat@ynov.com>
2020-11-11 11:28:23 +0100

    ✨ Add basic App Bar with search field

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
+import { AppBar, Grid, Paper } from '@material-ui/core';
 import './App.css';
 import User from './components/user'
+import Header from './components/header'
 
 function App() {
   return (
     <div className="App">
-      <User></User>
+      <Header></Header>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <Paper>
+            <User></User>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam quod, sapiente molestias optio labore ad incidunt voluptatibus alias fugiat odit natus eveniet nemo amet neque! Cupiditate, sapiente. Tenetur, adipisci commodi!</Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 }
